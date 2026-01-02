94eaa5db42f4088c953d97fbe143462ea1759664
Davuluri Harsha <davuluri.harsha@epsilon.com>
2026-01-02 15:44:26 +0530

    gen ai beginning

---
+++ gen-ai.py
@@ -0,0 +1,12 @@
+'''
+
+Generative AI
+
+
+Data objects -> Deep Neural Networks -> Taks
+
+
+Data objects like Data, text, audio etc are inputs which are passed to DNN to understand the patterns
+and perform tasks like create image, convert image to text etc.
+
+'''
