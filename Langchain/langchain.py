e94517d9d4b1c234c714e1961174c897bb408a89
Davuluri Harsha <manoharsha44@gmail.com>
2026-04-20 10:35:40 +0530

    restructuring

---
+++ Langchain/langchain.py
@@ -0,0 +1,23 @@
+'''
+
+LangChain is a framework to start building completely custom agents and applications powered by LLMs.
+
+
+    With Under 10 lines of code, we can connect to OpenAI, Anthropic, Google, and more.
+
+    
+    Let's connect to one of the model hosted on Databricks with the help of LangChain
+
+    
+
+    package required : databricks-langchain
+
+    %pip install databricks-langchain
+
+
+
+
+
+
+
+'''
