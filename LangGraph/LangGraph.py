e5490d5c3e10939ecee1ea3af1c87850877807dd
harshadavuluri44 <manoharsha44@gmail.com>
2026-06-05 09:53:10 +0530

    Repo restructuring

---
+++ LangGraph/LangGraph.py
@@ -0,0 +1,18 @@
+'''
+
+LangGraph overview
+
+
+    LangGraph is a low-level orchestration framework and runtime for building, managing, and deploying long-running, stateful agents.
+
+
+Stateful means - system remembers past interactions or data, rather than starting fresh every time.
+    
+A stateful agent is like a personal assistant who remembers your preferences and past conservations, while a stateless one is like a call center operator who
+treats every call as brand new.
+
+
+
+
+
+'''
