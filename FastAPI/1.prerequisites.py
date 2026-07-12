619c8562a25f1c8908b26016fb4bf65bb36bfdd9
harshadavuluri44 <manoharsha44@gmail.com>
2026-07-12 12:25:28 +0530

    restructuring

---
+++ FastAPI/1.prerequisites.py
@@ -0,0 +1,44 @@
+'''
+
+CONSOLE BASED APPLICATION is a program that runs inside a terminal or command-line interface (CLI) instead of having a graphical user interface (GUI) with
+windows, buttons or menus.
+
+
+Characteristics of Console-Based Applications:
+
+- Text-only interface: You interact with the program by typing commands or reading text output.
+- Runs in terminal: Words inside tools like Command Prompt, Terminal or PowerShell.
+- Keyboard-driven: Input is typed directly, no mouse clicks or graphical elements.
+- Lightweight: Uses fewer system resources compared to GUI apps.
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+GUI (GRAPHICAL USER INTERFACE) building means creating applications that people can interact with using visual elements like windows, buttons, menus, icons,
+and text boxes - instead of typing commands in a console.
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+WEB FRAMEWORK is a set of tools and libraries that helps developers build websites or web applications more easily, without having to write everything
+from scratch. It provides a structured way to handle common tasks like ROUTING, REQUEST HANDLING, TEMPLATES and DATABASE CONNECTIONS.
+
+
+What WEB FRAMEWORKS DO?
+
+- ROUTING: Decide which function should run when a user visits a certain URL (e.g., /assets, /asset_id)
+
+- REQUEST HANDLING: Process incoming HTTP requests (like GET, POST) and send back responses.
+
+- DATABASE INTEGRATION: Provides tools to connect and interact with databases.
+
+- SECURITY FEATURES: Handle authentication, authorization, and protection against common attacks.
+
+
+Examples of web framewroks: Django, Flask, FastAPI, Express.js, Ruby on Rails, Sprint, etc.
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+Using FastAPI we can backend of our application, user Js, React for frotend.
+
+
+'''
--- zeneric_knowledge/module_package_library.py
+++
@@ -1,9 +0,0 @@
-'''
-
-Module - A single .py file (e.g., os.py, hashlib.py etc)
-
-Package - A directory with an __init__.py containing submodules.
-
-
-
-'''
