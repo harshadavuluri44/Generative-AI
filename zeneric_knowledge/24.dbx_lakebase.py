4137d6e9e9455dee9b9a56f13ecb3cc57f9a1ae0
Mano Harsha Davuluri <manoharsha44@gmail.com>
2026-08-11 10:49:24 +0530

    info on lakebase

---
+++ zeneric_knowledge/24.dbx_lakebase.py
@@ -0,0 +1,19 @@
+'''
+
+create a project : datahub_mcp
+
+branch by default : production
+
+by default db get's created: databricks_postgres (same name in all projects)
+
+
+whatever the table we create it gets created in the databricks_postgres db unless we specify otherwise.
+
+
+
+LAKEBASE_ENDPOINT is used to generate oauth token.
+
+LAKEBASE_HOST and above token is setup connection to the lakebase 
+
+
+'''
